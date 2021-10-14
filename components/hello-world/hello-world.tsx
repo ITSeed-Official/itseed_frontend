cb275a64474e62db426e3d12b0c6cdd2536c0576
nick chen <nick@nickde-MacBook-Pro.local>
2021-10-14 22:57:21 +0800

    temp

--- components/hello-world/hello-world.tsx
+++ components/hello-world/hello-world.tsx
@@ -6,6 +6,8 @@ const HelloWorld = ({ name }: Props) => {
   return (
     <p style={{ padding: "24px", fontSize: "16px" }}>
       Hello {name}, how are you?
+      <br/>
+      Hello World
     </p>
   );
 };
