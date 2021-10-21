e8f191231025c36d396037ab970f26e1192aa150
Malik705017 <fhh1482@gmail.com>
2021-10-21 13:11:24 +0800

    feat: add Banner, Button, Card components

---
+++ components/atoms/Banner/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import styles from "./index.module.css";
+
+const Banner: FC = () => {
+  return <div className={styles.banner}></div>;
+};
+
+export default Banner;
---
+++ components/atoms/Card/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+import { SubTitle } from "../SectionTitle";
+
+import styles from "./index.module.css";
+
+const Card: FC = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardImg} />
+      <div className={styles.cardInfo}>
+        <SubTitle title="「資種帶你飛」" className={styles.cardTitle} />
+        <p className={styles.authorInfo}>XX大學 XXX</p>
+        <p>資訊種子培訓計畫不同於大學一貫的授課方式，讓學員在實踐中學習。</p>
+        <p>
+          透過執行 4 大專案，參與 10+
+          堂來自業界講師的課程，了解業界生態，並探索自己未來的職涯方向，培養跨領域合作、解決問題的思維等職場必備的能力，成為能踏入職場的人才!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
