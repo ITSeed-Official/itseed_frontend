28d90ae87096c645d3665203f6f0b9321e551b7e
nick chen <nick@nickde-MacBook-Pro.local>
2021-10-28 01:13:35 +0800

    feat: integrate with sentry.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+
+let SENTRY_AUTO_RELEASE = process.env.SENTRY_AUTO_RELEASE === 'true';
+
+const moduleExports = {
   reactStrictMode: true,
-}
+  sentry: {
+    disableServerWebpackPlugin: !SENTRY_AUTO_RELEASE,
+    disableClientWebpackPlugin: !SENTRY_AUTO_RELEASE,
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
