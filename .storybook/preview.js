728a9b16955b8eacbc68a2f85ffa3960ec4189e2
Malik705017 <fhh1482@gmail.com>
2021-11-04 15:23:51 +0800

    feat: add sass and sass loader to enable scss in storybook and website

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 // Import the global style enabling tailwind classes
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
