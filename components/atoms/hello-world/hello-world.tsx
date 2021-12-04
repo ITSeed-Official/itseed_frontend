64a52ab7d84054daeec794b3487542798e59bb3b
nick chen <nick@nickde-MacBook-Pro.local>
2021-12-04 13:07:32 +0800

    feat: stop testing for efficiency

--- components/atoms/hello-world/hello-world.tsx
+++
@@ -1,9 +0,0 @@
-export type Props = {
-  name: string;
-};
-
-const HelloWorld = ({ name }: Props) => {
-  return <p style={{ padding: "24px", fontSize: "12px" }}>Hello {name}</p>;
-};
-
-export default HelloWorld;
