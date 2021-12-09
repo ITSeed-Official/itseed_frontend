cb14b993a5cc07284b96094f672b02fd6f1d610e
Malik705017 <fhh1482@gmail.com>
2021-12-09 18:58:22 +0800

    refactor: split homepage into different section components

---
+++ pages/home/TimelineSection/TimelineSection.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+
+import SectionWrapper from "components/molecules/SectionWrapper";
+
+import styles from "./TimelineSection.module.scss";
+
+const TimelineSection: FC = () => {
+  return (
+    <div className={styles.timelineSection}>
+      <SectionWrapper title="招募流程與時程">
+        <div className={styles.plan} />
+      </SectionWrapper>
+    </div>
+  );
+};
+
+export default TimelineSection;
