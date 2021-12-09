91fd8754ea93449a1e9bf5bd42f2be196d5bcb4c
Malik705017 <fhh1482@gmail.com>
2021-12-09 19:01:21 +0800

    fix: revise Card component to use data from parent

--- components/molecules/Card/Card.tsx
+++ components/molecules/Card/Card.tsx
@@ -1,26 +1,49 @@
 import { FC } from "react";
 import classnames from "classnames";
+import Image from "next/image";
 
-import { SubTitle } from "../../atoms/SectionTitle";
+import ReadmoreIcon from "public/images/common/icons/readmore.svg";
 
 import styles from "./Card.module.scss";
 
 type CardProperty = {
   className?: string;
+  imgSrc?: string;
+  content: string;
+  name: string;
+  character: string;
+  readmore?: string;
 };
 
-const Card: FC<CardProperty> = ({ className }) => {
+const Card: FC<CardProperty> = ({
+  className,
+  imgSrc,
+  content,
+  name,
+  character,
+  readmore = "看更多"
+}) => {
   return (
     <div className={classnames(styles.card, className)}>
-      <div className={styles.cardImg} />
+      <div
+        className={styles.cardImg}
+        style={
+          imgSrc
+            ? {
+                backgroundImage: `url(${imgSrc})`,
+                backgroundSize: "cover"
+              }
+            : { backgroundColor: "gray" }
+        }
+      />
       <div className={styles.cardInfo}>
-        <SubTitle title="「資種帶你飛」" className={styles.cardTitle} />
-        <p className={styles.authorInfo}>XX大學 XXX</p>
-        <p>資訊種子培訓計畫不同於大學一貫的授課方式，讓學員在實踐中學習。</p>
-        <p>
-          透過執行 4 大專案，參與 10+
-          堂來自業界講師的課程，了解業界生態，並探索自己未來的職涯方向，培養跨領域合作、解決問題的思維等職場必備的能力，成為能踏入職場的人才!
-        </p>
+        <p className={styles.content}>{content}</p>
+        <p>{name}</p>
+        <p>{character}</p>
+        <div className={styles.readmore}>
+          <p>{readmore}</p>
+          <Image src={ReadmoreIcon} alt="readmore" />
+        </div>
       </div>
     </div>
   );
