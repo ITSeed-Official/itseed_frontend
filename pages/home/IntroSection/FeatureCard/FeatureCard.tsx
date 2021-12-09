f526f864d006cbd828d310a55a51ce0cbd60d379
Malik705017 <fhh1482@gmail.com>
2021-12-09 19:00:25 +0800

    refactor: use FeatureCard to make code cleaner

---
+++ pages/home/IntroSection/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,26 @@
+import type { FC } from "react";
+import { SubTitle } from "components/atoms/SectionTitle";
+
+import styles from "./FeatureCard.module.scss";
+
+type FeatureCardProperty = {
+  imgSrc: string;
+  title: string;
+  content: string[];
+};
+
+const FeatureCard: FC<FeatureCardProperty> = ({ imgSrc, title, content }) => {
+  return (
+    <div className={styles.feature}>
+      <img className={styles.featureImg} src={imgSrc} alt={title} />
+      <SubTitle title={title} className={styles.featureTitle} />
+      <div className={styles.featureContent}>
+        {content.map((text, index) => (
+          <p key={`${text}_${index}`}>{text}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeatureCard;
