f30b6335e074a6db4ecd62367161f9272bd1f1da
alansyue <b123105@gmail.com>
2021-12-12 13:30:30 +0800

    refactor: let Banner can customize

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -9,7 +9,14 @@ import FAQSection from "components/templates/Home/FAQSection";
 
 const Home: NextPage = () => (
   <>
-    <BannerContainer backgroundImage="images/homepage/pics/banner.png" />
+    <BannerContainer
+      backgroundImage="images/homepage/pics/banner.png"
+      bigTitle="擁抱每一份獨特"
+      subTitle="第十九屆資訊種子培訓計畫"
+      subText="報名時間：2022/6/1-7/1"
+      buttonText="立即報名"
+      showSummaryInfo={true}
+    />
     <SummarySection />
     <IntroSection />
     <AlumniSection />
