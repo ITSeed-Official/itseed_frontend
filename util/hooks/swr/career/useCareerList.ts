9cb04a4c0f96c844aede8fd9b9f21547c61ed87f
Malik705017 <fhh1482@gmail.com>
2022-01-23 16:11:38 +0800

    feat: add career api and swr hook

---
+++ util/hooks/swr/career/useCareerList.ts
@@ -0,0 +1,28 @@
+import useSWR from "swr";
+import axios, { AxiosError } from "axios";
+
+import { CareerExperience } from "api/careers";
+
+type APIResponse = CareerExperience[];
+
+interface Response {
+  careerExperiences: CareerExperience[];
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const domain = process.env.NEXT_PUBLIC_API_ORIGIN;
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+export const useCareerList = (): Response => {
+  const { data, error } = useSWR<APIResponse, AxiosError>(
+    `${domain}/careers`,
+    fetcher
+  );
+
+  return {
+    careerExperiences: data || [],
+    isLoading: !error && !data,
+    isError: !!error,
+  };
+};
