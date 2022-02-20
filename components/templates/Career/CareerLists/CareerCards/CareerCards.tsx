1a5c53cfa5afed42054bfff84492b55cf41e7886
Malik705017 <fhh1482@gmail.com>
2022-02-20 09:38:12 +0800

    fix: use CareerCard to make code clean in CareerCards and CareerLists

--- components/templates/Career/CareerLists/CareerCards/CareerCards.tsx
+++ components/templates/Career/CareerLists/CareerCards/CareerCards.tsx
@@ -1,27 +1,23 @@
 import type { FC } from 'react';
-import { useRouter } from 'next/router';
 
 import { useMedia } from 'util/hooks/useMedia';
 import { chunkArray } from 'util/helper';
-import { appPath } from 'util/routing.config';
 import type { ExperienceCategory } from 'api/careers';
 
 import SectionWrapper from 'components/molecules/SectionWrapper';
-import Card from 'components/molecules/Card';
-import type { CareerCardProperty } from '../CareerLists';
+import CareerCard, { CareerCardType } from './CareerCard';
 
 import styles from './CareerCards.module.scss';
 
 interface CareerCardsProperty {
   className: string;
-  careerCards: CareerCardProperty[];
+  careerCards: CareerCardType[];
   category: ExperienceCategory;
   title: string;
 }
 
 const CareerCards: FC<CareerCardsProperty> = ({ className, careerCards, category, title }) => {
   const media = useMedia();
-  const router = useRouter();
 
   const show = {
     desktop: 3,
@@ -35,37 +31,19 @@ const CareerCards: FC<CareerCardsProperty> = ({ className, careerCards, category
         {chunkArray(careerCards, show[media]).map((cards, index) => (
           <div className={styles.cards} key={index}>
             {cards.map(({ id, company, job, role, name, description, imgSrc, mentorsOverview }) => (
-              <Card
+              <CareerCard
                 key={id}
+                id={id}
                 wrapperClassName={styles.card}
+                company={company}
+                job={job}
+                role={role}
                 name={name}
-                character={role}
-                content={description}
-                imgSrc={imgSrc || '/images/common/pics/alumni2.png'}
-                onClickCta={() => router.push(`${appPath.career}/${id}`)}
-              >
-                {category === 'company' && (
-                  <div className={styles.jobInfo}>
-                    {company && <p className={styles.cardTitle}>{company}</p>}
-                    <p className={styles.cardSubTitle}>{job}</p>
-                  </div>
-                )}
-                {category === 'interview' && (
-                  <div className={styles.mentorInfo}>
-                    <p className={styles.cardTitle}>{job}</p>
-                    <p className={styles.mentorsOverview}>{`訪談對象：${mentorsOverview}`}</p>
-                  </div>
-                )}
-                {category === 'personalization' && (
-                  <div>
-                    <div className={styles.jobInfo}>
-                      <p className={styles.cardTitle}>{name}</p>
-                      <p className={styles.cardSubTitle}>{job}</p>
-                    </div>
-                    <p className={styles.mentorsOverview}>{`Mentor: ${mentorsOverview}`}</p>
-                  </div>
-                )}
-              </Card>
+                description={description}
+                imgSrc={imgSrc}
+                mentorsOverview={mentorsOverview}
+                category={category}
+              />
             ))}
           </div>
         ))}
