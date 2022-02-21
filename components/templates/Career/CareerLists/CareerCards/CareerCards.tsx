1476ee4680033c268203313857d9870f18a008bb
Malik705017 <fhh1482@gmail.com>
2022-02-21 10:55:31 +0800

    feat: use css grid for CareerCards

--- components/templates/Career/CareerLists/CareerCards/CareerCards.tsx
+++ components/templates/Career/CareerLists/CareerCards/CareerCards.tsx
@@ -1,8 +1,6 @@
 import type { FC } from 'react';
 
-import { useMedia } from 'util/hooks/useMedia';
 import { chunkArray } from 'util/helper';
-import type { ExperienceCategory } from 'api/careers';
 
 import SectionWrapper from 'components/molecules/SectionWrapper';
 import CareerCard, { CareerCardType } from './CareerCard';
@@ -16,36 +14,26 @@ interface CareerCardsProperty {
 }
 
 const CareerCards: FC<CareerCardsProperty> = ({ className, careerCards, title }) => {
-  const media = useMedia();
-
-  const show = {
-    desktop: 3,
-    tablet: 2,
-    mobile: 1,
-  };
-
   return (
     <div className={className}>
       <SectionWrapper className={styles.cardsSection} title={title} titleClassName={styles.title}>
-        {chunkArray(careerCards, show[media]).map((cards, index) => (
-          <div className={styles.cards} key={index}>
-            {cards.map(({ id, company, category, job, role, name, description, imgSrc, mentorsOverview }) => (
-              <CareerCard
-                key={id}
-                id={id}
-                wrapperClassName={styles.card}
-                company={company}
-                job={job}
-                role={role}
-                name={name}
-                description={description}
-                imgSrc={imgSrc}
-                mentorsOverview={mentorsOverview}
-                category={category}
-              />
-            ))}
-          </div>
-        ))}
+        <div className={styles.cards}>
+          {careerCards.map(({ id, company, category, job, role, name, description, imgSrc, mentorsOverview }) => (
+            <CareerCard
+              key={id}
+              id={id}
+              wrapperClassName={styles.card}
+              company={company}
+              job={job}
+              role={role}
+              name={name}
+              description={description}
+              imgSrc={imgSrc}
+              mentorsOverview={mentorsOverview}
+              category={category}
+            />
+          ))}
+        </div>
       </SectionWrapper>
     </div>
   );
