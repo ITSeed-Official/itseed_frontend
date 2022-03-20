8f08a345b7f6508a6714efe63e1d0503af60f009
Malik705017 <fhh1482@gmail.com>
2022-03-20 11:08:20 +0800

    feat: add courses api and courses page

---
+++ api/courses.ts
@@ -0,0 +1,44 @@
+const domain = process.env.NEXT_PUBLIC_API_ORIGIN;
+
+export type CourseDetailType = {
+  id: string;
+  title: string;
+  subtitle: string;
+  overview: string;
+  lecturer: string;
+  lecturer_company: string;
+  lecturer_position: string;
+  lecturer_background: string;
+  agenda: string;
+  experience: string;
+  sharer: string;
+  sharer_education: string;
+  published_at: string;
+  created_at: string;
+  updated_at: string;
+  image: {
+    id: string;
+    name: string;
+    url: string;
+  };
+};
+
+export const getCourses = async () => {
+  try {
+    const response = await fetch(`${domain}/courses`);
+    const data: CourseDetailType[] = await response.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export const getCourseDetail: (id: string) => Promise<CourseDetailType> = async (id) => {
+  try {
+    const response = await fetch(`${domain}/courses/${id}`);
+    const data: CourseDetailType = await response.json();
+    return data;
+  } catch (error) {
+    return {};
+  }
+};
---
+++ pages/courses/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage, GetStaticProps } from 'next';
+
+import { getSeos } from 'api/seos';
+import { getCourses, CourseDetailType } from 'api/courses';
+
+import CoursesLists from 'components/templates/Courses/CoursesLists';
+import PageMeta from 'components/atoms/PageMeta';
+
+const Courses: NextPage<{ courses: CourseDetailType[] }> = ({ courses }) => {
+  return <CoursesLists courses={courses} />;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const [meta, courses] = await Promise.all([getSeos('seo'), getCourses()]);
+
+  return {
+    props: {
+      meta,
+      courses,
+    },
+    revalidate: 1,
+  };
+};
+
+export default PageMeta(Courses);
