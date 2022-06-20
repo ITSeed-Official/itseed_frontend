3254ddb2bd9fd698bb19a94cd348c4b8181f61c3
Malik705017 <fhh1482@gmail.com>
2022-06-20 23:29:39 +0800

    refactor: revise Recruit page

--- pages/recruit/index.tsx
+++ pages/recruit/index.tsx
@@ -1,15 +1,18 @@
+import { GetStaticProps } from 'next';
+
 import Recruit from 'components/templates/Recruit';
 import PageMeta from 'components/atoms/PageMeta';
 import { getSeos } from 'api/seos';
 
-export async function getServerSideProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const [meta] = await Promise.all([getSeos('recruit')]);
 
   return {
     props: {
       meta,
     },
+    revalidate: 1,
   };
-}
+};
 
 export default PageMeta(Recruit);
