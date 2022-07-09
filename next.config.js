82928fee25b2149bdf3f8b3e85985635917c2abf
nick chen <nick@MacBook-Pro.local>
2022-07-09 14:45:35 +0800

    fix: cdn issue

--- next.config.js
+++ next.config.js
@@ -5,18 +5,17 @@
 
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const isProd = process.env.NODE_ENV === 'production'
 const SENTRY_AUTO_RELEASE = process.env.SENTRY_AUTO_RELEASE === 'true';
 
 const moduleExports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn1.itseed.tw' : '',
+  assetPrefix: '', // CDN Origin
   sentry: {
     disableServerWebpackPlugin: !SENTRY_AUTO_RELEASE,
     disableClientWebpackPlugin: !SENTRY_AUTO_RELEASE,
   },
   images: {
-    domains: ['www.itseed.tw', 'dev.itseed.tw', 'd1pahwu47gk4xf.cloudfront.net'],
+    domains: ['www.itseed.tw', 'dev.itseed.tw', 'd1pahwu47gk4xf.cloudfront.net', 'cdn1.itseed.tw'],
   },
 };
 
