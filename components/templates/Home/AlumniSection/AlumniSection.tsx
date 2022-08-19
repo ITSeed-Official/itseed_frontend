67a0c83c0b77258f9bb1f5f7fe21fc88d3916f79
alansyue <b123105@gmail.com>
2022-08-19 09:09:47 +0800

    refactor: adjust alumnis card in homepage

--- components/templates/Home/AlumniSection/AlumniSection.tsx
+++ components/templates/Home/AlumniSection/AlumniSection.tsx
@@ -13,51 +13,46 @@ import styles from './AlumniSection.module.scss';
 const data = [
   {
     id: 1,
-    name: '賴一霖1',
-    img: '/images/common/pics/alumni.png',
+    name: '蕭程澤',
+    img: '/images/common/pics/alumni.jpeg',
     content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
-    character: '第18屆副學員長',
+      '每個專案帶給我的成長不同，有的我與夥伴發想，探討看見的問題；有的我與夥伴共創，實地行動，落實想法。來找吧，屬於你的寶藏都在這了！',
+    character: '19th 職涯專案總召',
   },
   {
     id: 2,
-    name: '賴一霖2',
-    img: '/images/common/pics/alumni2.png',
-    content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
-    character: '第18屆副學員長',
+    name: '羅元駿',
+    img: '/images/common/pics/alumni2.jpeg',
+    content: '在資種，一同成長蛻變的夥伴、鼓勵積極態度的環境、熱心付出的學長姐，種種美好都不自覺地讓我愛上這個地方。',
+    character: '19th 學員',
   },
   {
     id: 3,
-    name: '賴一霖3',
-    img: '/images/common/pics/alumni3.png',
-    content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
+    name: '陳泓文',
+    img: '/images/common/pics/alumni3.jpeg',
+    content: '資種的各種課程，是引領我的必要羅盤，幫助我了解職涯的航道該怎麼行駛，該怎麼塑造想要成為的自己。',
     character: '第18屆副學員長',
   },
   {
     id: 4,
-    name: '賴一霖4',
-    img: '/images/common/pics/alumni.png',
-    content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
-    character: '第18屆副學員長',
+    name: '陳思穎',
+    img: '/images/common/pics/alumni4.jpeg',
+    content: '我們永遠無法用相同的自己，得到不一樣的未來',
+    character: '19th 學員長',
   },
   {
     id: 5,
-    name: '賴一霖5',
-    img: '/images/common/pics/alumni2.png',
-    content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
-    character: '第18屆副學員長',
+    name: '郭宜臻',
+    img: '/images/common/pics/alumni5.jpeg',
+    content: '也許困惑迷惘，但這條路不再孤單',
+    character: '19th 副學員長',
   },
   {
     id: 6,
-    name: '賴一霖6',
-    img: '/images/common/pics/alumni3.png',
-    content:
-      '「我曾經被學長姐無私的回饋而感動，那種認真聽、耐心答、樂於分享的學長姐，不但沒有距離，更是我們未來前進的力量！」',
-    character: '第18屆副學員長',
+    name: '蘇如禧',
+    img: '/images/common/pics/alumni6.jpeg',
+    content: '在資種，擁有豐富的資源、寬廣的舞台、優秀的夥伴，等著你探索自我的價值所在。',
+    character: '19th 行銷長',
   },
 ];
 
