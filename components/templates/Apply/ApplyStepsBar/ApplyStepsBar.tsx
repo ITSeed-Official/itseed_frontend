a6b2dacad8d1bdbf76e660a5ec2d288c1d1a7639
Malik705017 <fhh1482@gmail.com>
2023-02-07 20:38:35 +0800

    feat: create ApplyStepsBar for steps progress show

---
+++ components/templates/Apply/ApplyStepsBar/ApplyStepsBar.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import classnames from 'classnames';
+import Icon from 'components/atoms/Icon';
+
+import styles from './ApplyStepsBar.module.scss';
+
+type ApplyStepsBarProps = {
+  curStep: number;
+};
+
+const steps = ['DISC', '個人資料', '書審問題', '檔案上傳', '前往繳費'];
+
+const ApplyStepsBar: FC<ApplyStepsBarProps> = ({ curStep = 0 }) => {
+  return (
+    <div className={styles.container}>
+      {steps.map((title, index) => (
+        <div key={title} className={classnames(styles.step, curStep === index && styles.curStep)}>
+          <h5 className={styles.title}>{title}</h5>
+          <div className={classnames(styles.stepIcon, curStep === index && styles.curStep)}>
+            <Icon width={36} height={36} iconSrc={`/images/apply/icons/step${index + 1}.svg`} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ApplyStepsBar;
