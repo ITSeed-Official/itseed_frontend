5aed23ce9623a0bf24b7a70b709ecab12e9c9d64
Malik705017 <fhh1482@gmail.com>
2023-02-09 16:00:07 +0800

    fix: change login UI in Apply page

--- components/templates/Apply/Apply.tsx
+++ components/templates/Apply/Apply.tsx
@@ -3,13 +3,15 @@ import { useContext } from 'react';
 import Link from 'next/link';
 
 import { AuthContext } from 'contexts/AuthContext';
+import { appPath } from 'util/routing.config';
 
 import TemplateWrapper from 'components/organisms/TemplateWrapper';
 import SectionWrapper from 'components/molecules/SectionWrapper';
 import Button from 'components/atoms/Button';
+import Icon from 'components/atoms/Icon';
 
 import styles from './Apply.module.scss';
-import { appPath } from 'util/routing.config';
+import Image from 'next/image';
 
 const Apply: NextPage<any> = () => {
   const { isLogin, nickname, signIn, signOut } = useContext(AuthContext);
@@ -21,6 +23,13 @@ const Apply: NextPage<any> = () => {
         <div className={styles.container}>
           {isLogin ? (
             <>
+              <Image
+                className={styles.image}
+                src={'/images/apply/pics/welcome.png'}
+                width={212}
+                height={212}
+                layout="fixed"
+              />
               <h2 className={styles.title}>{nickname}，您好！</h2>
               <p className={styles.description}>
                 歡迎來到資訊種子培訓計畫報名頁面！接下來將邀請您填寫報名相關資訊，
@@ -36,11 +45,18 @@ const Apply: NextPage<any> = () => {
           ) : (
             <>
               <h2 className={styles.title}>登入</h2>
-              <p className={styles.description}>邀請您先登入Ｇoogle帳號以利報名作業進行。</p>
-              <Button onClick={() => signIn()}>Sign in with Google</Button>
+              <p className={styles.description}>邀請您先登入 Google 帳號以利報名作業進行。</p>
+              <Button onClick={() => signIn()}>
+                <Icon className={styles.loginIcon} iconSrc={'/images/icons/icons-google-login.svg'} />
+                Sign in with Google
+              </Button>
             </>
           )}
         </div>
+        <p>
+          目前僅開放Google帳號註冊/登入，如有任何其他相關問題，請私訊 資訊種子粉絲專頁，或寄信至 ITseed
+          信箱（itseed19th@gmail.com）將由專人為您解答！
+        </p>
       </SectionWrapper>
     </TemplateWrapper>
   );
