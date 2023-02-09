9b79755d642e9885028bb24b65a14a39d0ec9cc4
Malik705017 <fhh1482@gmail.com>
2023-02-09 15:37:19 +0800

    fix: remove unused api

--- api/auth.ts
+++ api/auth.ts
@@ -1,19 +1,4 @@
 import { API2_DOMAIN } from 'util/const';
-import { ErrorCode, ErrorWithCode } from 'util/error';
-
-export const authCheck = async (): Promise<any> => {
-  try {
-    await fetch(`${API2_DOMAIN}/users/me`, { credentials: 'include' });
-    return true;
-  } catch (e) {
-    if (e instanceof ErrorWithCode) {
-      if (e.errorCode === ErrorCode.NoAuth) {
-        return false;
-      }
-      throw e;
-    }
-  }
-};
 
 export const logout = async (): Promise<any> => {
   const response = await fetch(`${API2_DOMAIN}/auth/logout`, { credentials: 'include', method: 'POST' });
