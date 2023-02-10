aa36f5b0e1abf8f650a358ee3124f716c35d2429
Malik705017 <fhh1482@gmail.com>
2023-02-10 20:44:19 +0800

    fix: make ApplyStepsBar icon anf title can clicked and go to specific page

--- components/templates/Apply/ApplyStepsBar/ApplyStepsBar.tsx
+++ components/templates/Apply/ApplyStepsBar/ApplyStepsBar.tsx
@@ -6,18 +6,26 @@ import styles from './ApplyStepsBar.module.scss';
 
 type ApplyStepsBarProps = {
   curStep: number;
+  goTo: Function;
 };
 
 const steps = ['DISC', '個人資料', '書審問題', '檔案上傳', '前往繳費'];
 
-const ApplyStepsBar: FC<ApplyStepsBarProps> = ({ curStep = 0 }) => {
+const ApplyStepsBar: FC<ApplyStepsBarProps> = ({ curStep = 0, goTo }) => {
   return (
     <div className={styles.container}>
       {steps.map((title, index) => (
         <div key={title} className={classnames(styles.step, curStep === index && styles.curStep)}>
-          <h5 className={styles.title}>{title}</h5>
+          <h5 className={styles.title} onClick={() => goTo(index)}>
+            {title}
+          </h5>
           <div className={classnames(styles.stepIcon, curStep === index && styles.curStep)}>
-            <Icon width={36} height={36} iconSrc={`/images/apply/icons/step${index + 1}.svg`} />
+            <Icon
+              onClick={() => goTo(index)}
+              width={36}
+              height={36}
+              iconSrc={`/images/apply/icons/step${index + 1}.svg`}
+            />
           </div>
         </div>
       ))}
