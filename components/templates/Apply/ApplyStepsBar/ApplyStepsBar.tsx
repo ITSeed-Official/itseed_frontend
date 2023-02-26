09411f0e5ac4e60239dd5d1c9db2e1436d4ca8d9
Malik705017 <fhh1482@gmail.com>
2023-02-26 14:24:48 +0800

    fix: disable jumper on stepBar

--- components/templates/Apply/ApplyStepsBar/ApplyStepsBar.tsx
+++ components/templates/Apply/ApplyStepsBar/ApplyStepsBar.tsx
@@ -1,39 +1,24 @@
 import { FC } from 'react';
 import classnames from 'classnames';
+
+import { stepMap } from 'util/form';
+
 import Icon from 'components/atoms/Icon';
 
 import styles from './ApplyStepsBar.module.scss';
 
 type ApplyStepsBarProps = {
   curStep: number;
-  goTo: Function;
-  updateStepForm: Function;
 };
 
-const steps = ['DISC', '基本資料', '書審問題', '檔案上傳', '前往繳費'];
-
-const ApplyStepsBar: FC<ApplyStepsBarProps> = ({ curStep = 0, goTo, updateStepForm }) => {
-  const onClickHandler = (index: number) => {
-    if (index === 4) return; // 繳費不能直接點選
-
-    updateStepForm();
-    goTo(index);
-  };
-
+const ApplyStepsBar: FC<ApplyStepsBarProps> = ({ curStep = 0 }) => {
   return (
     <div className={styles.container}>
-      {steps.map((title, index) => (
+      {stepMap.map((title, index) => (
         <div key={title} className={classnames(styles.step, curStep === index && styles.curStep)}>
-          <h5 className={styles.title} onClick={() => onClickHandler(index)}>
-            {title}
-          </h5>
+          <h5 className={styles.title}>{title}</h5>
           <div className={classnames(styles.stepIcon, curStep === index && styles.curStep)}>
-            <Icon
-              onClick={() => onClickHandler(index)}
-              width={36}
-              height={36}
-              iconSrc={`/images/apply/icons/step${index + 1}.svg`}
-            />
+            <Icon width={36} height={36} iconSrc={`/images/apply/icons/step${index + 1}.svg`} />
           </div>
         </div>
       ))}
