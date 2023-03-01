2f79da45334710e11ada09ff519be776522c20d7
Malik705017 <fhh1482@gmail.com>
2023-03-01 11:19:29 +0800

    feat: create ModalSave component to show save status

---
+++ components/molecules/ModalSave/ModaSavel.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+import Modal from '../Modal/Modal';
+import Icon from 'components/atoms/Icon';
+
+import styles from './ModalSave.module.scss';
+
+type ModalProps = {
+  className?: string;
+  closeModal?: Function;
+  isFormUpdateSuccess: boolean;
+};
+
+const ModalSave: FC<ModalProps> = ({ closeModal = () => {}, isFormUpdateSuccess = false }) => {
+  return (
+    <Modal noBackDrop autoClose closeModal={closeModal} className={styles.modalSave}>
+      {isFormUpdateSuccess ? (
+        <>
+          <Icon className={styles.saveIcon} iconSrc="/images/icons/icon-checkcircle.svg" />
+          儲存成功！
+        </>
+      ) : (
+        <>
+          <Icon className={styles.saveIcon} iconSrc="/images/icons/icon-cancel.svg" />
+          儲存失敗
+        </>
+      )}
+    </Modal>
+  );
+};
+
+export default ModalSave;
