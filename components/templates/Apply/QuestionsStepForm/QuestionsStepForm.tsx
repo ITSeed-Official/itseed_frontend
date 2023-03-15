3773d456e3465206ebaf7c69b1a86473b5fffe08
Alan Hsueh <alan.hsueh@shoplineapp.com>
2023-03-15 10:24:00 +0800

    feat: remove the interview question example download hint

--- components/templates/Apply/QuestionsStepForm/QuestionsStepForm.tsx
+++ components/templates/Apply/QuestionsStepForm/QuestionsStepForm.tsx
@@ -28,7 +28,6 @@ const QuestionsStepForm: FC<QuestionsStepFormProps> = ({ data, updateFields }) =
     <>
       <div className={styles.intro}>
         <p>本階段共有六道題目，建議花30-40分鐘的時間填寫。</p>
-        <p>※可從此處下載文字檔書審題目。</p>
       </div>
       <>
         {data.map(({ number, title, answer, limit }) => (
