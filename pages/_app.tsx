30bc9b777d963c016912fa237999ebfc4b2bdc8e
Alan Hsueh <alan.hsueh@shoplineapp.com>
2023-04-15 12:46:21 +0800

    feat: init GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 import { LayoutContextProvider } from 'contexts/LayoutContext';
 import { AuthProvider } from 'contexts/AuthContext';
 
 import Layout from 'components/organisms/Layout';
 
+import TagManager from 'react-gtm-module';
+
 import './inject.scss';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const gtmId = process.env.GTM_ID ?? '';
+    TagManager.initialize({ gtmId });
+  }, []);
+
   return (
     <AuthProvider>
       <LayoutContextProvider>
