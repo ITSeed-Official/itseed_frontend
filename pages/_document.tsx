c00fcf166d6be5edc6b38b217bd2bc35959ac0a9
Alan Hsueh <alan.hsueh@shoplineapp.com>
2023-04-21 17:37:51 +0800

    feat: replace image and add google search

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,700;1,400&family=Noto+Sans+TC:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+          <meta name="google-site-verification" content="euxQfgB1kocft1aAih9vgsEUkdw4Ljk2wGJHKLzzPUk" />
         </Head>
         <body>
           <Main />
