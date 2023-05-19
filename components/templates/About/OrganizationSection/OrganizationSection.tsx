d54c2aa30b42cbb8b0d4262c96a45503862aca62
Malik705017 <fhh1482@gmail.com>
2023-05-19 16:33:59 +0800

    fix: update About page content

--- components/templates/About/OrganizationSection/OrganizationSection.tsx
+++ components/templates/About/OrganizationSection/OrganizationSection.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import SectionWrapper from 'components/molecules/SectionWrapper';
 import Icon from 'components/atoms/Icon';
 import sprout from 'public/images/common/icons/sprout.png';
+import { CURRENT_SESSION, FB_PAGE, IG_PAGE } from 'util/const';
 
 import styles from './OrganizationSection.module.scss';
 
@@ -17,7 +18,7 @@ const OrganizationSection: FC = () => {
           </div>
           <hr />
           <p>
-            從2003年至今已累積 500
+            從 2003 年至今已累積 600
             多位校友，整合公會與校友資源，指引學員執行專案時突破自我，並傳承資訊種子分享與回饋的精神。
           </p>
         </div>
@@ -36,12 +37,20 @@ const OrganizationSection: FC = () => {
           </div>
           <hr />
           <p>
-            由當屆學員選舉產生，主要負責凝聚學員情感，以及團隊溝通協調。第十八屆幹部團隊包含：
+            由當屆學員選舉產生，主要負責凝聚學員情感，以及團隊溝通協調。第 {CURRENT_SESSION} 屆幹部團隊包含：
             <ul>
-              <li>正/副學員長：帶領幹部團隊進行年度規劃，與培訓部溝通合作，凝聚當屆的向心力。</li>
-              <li>資訊長：帶領資訊組負責當屆官網前後端資料的維護、整合，並開創工作坊以累積學員資訊能力。</li>
-              <li>設計長：帶領設計組設計當屆主視覺，產出周邊產品，留下獨一無二的紀念。</li>
-              <li>行銷長：帶領行銷組建立對外的傳播形象，與紀錄學員一年培訓過程，經手管道如粉絲專頁 Medium 等。</li>
+              <li>正/副學員長：促使學員彼此交流互動，共同成長，打造該屆的團隊文化，凝聚彼此感情。</li>
+              <li>
+                正/副行銷長：帶領行銷部建立對外形象，並紀錄學員一年的培訓過程，經營管道包含{' '}
+                <a className={styles.link} href={FB_PAGE} target="_blank">
+                  Facebook
+                </a>
+                、
+                <a className={styles.link} href={IG_PAGE} target="_blank">
+                  Instagram
+                </a>{' '}
+                等粉絲專頁 。
+              </li>
             </ul>
           </p>
           <p>（※每一屆幹部團隊職位依當屆培訓部規劃而有所不同※）</p>
