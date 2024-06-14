dfbec001fad0ac4f114aa3e4b29bd3242236d3c5
Alan Hsueh <alan.hsueh@shoplineapp.com>
2024-06-14 21:38:27 +0800

    feat: modify interview date

--- components/templates/Recruit/Information/data.ts
+++ components/templates/Recruit/Information/data.ts
@@ -9,5 +9,5 @@ export const informations: Information[] = [
   { title: '培訓地點', description: '市政府站附近之租借場地' },
   { title: '報名時間', description: '2024/05/24（五）00:00   至   2024/06/30（日）23:59' },
   { title: '培訓費用', description: '一年 3000 元' },
-  { title: '甄選流程', description: '資料初審 → 公布面試名單（7/22） → 團體面試（7/29、7/30） → 公布錄取（預計8月）' },
+  { title: '甄選流程', description: '資料初審 → 公布面試名單（7/22） → 團體面試（7/27、7/28） → 公布錄取（預計8月）' },
 ];
